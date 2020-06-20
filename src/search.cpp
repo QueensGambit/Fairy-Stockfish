4e3bb15a60a68e2e2a4adea911e1d4aaf5bb9e11
Fabian Fichter <ianfab@users.noreply.github.com>
2020-06-20 12:48:59 +0200

    Tweak capture SEE pruning margin for CTF variants

--- src/search.cpp
+++ src/search.cpp
@@ -1111,7 +1111,7 @@ moves_loop: // When in check, search starts from here
               if (!pos.see_ge(move, Value(-(32 - std::min(lmrDepth, 18)) * lmrDepth * lmrDepth)))
                   continue;
           }
-          else if (!pos.see_ge(move, Value(-194 - 120 * pos.captures_to_hand()) * depth)) // (~25 Elo)
+          else if (!pos.see_ge(move, Value(-194 - 120 * pos.captures_to_hand() - 20 * !!pos.capture_the_flag_piece()) * depth)) // (~25 Elo)
               continue;
       }
 
